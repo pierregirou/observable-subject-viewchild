236cd9f508e99c28418559fa10f00b72d35f4ddc
pierregirou <pierre.girou42290@gmail.com>
2021-08-01 09:55:06 +0200

    using .pipe RXJS

--- src/app/enfant/enfant.component.ts
+++ src/app/enfant/enfant.component.ts
@@ -1,4 +1,11 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject, from } from 'rxjs';
+import { filter, map } from 'rxjs/operators'
+/**interface créer pour l'exemple du pipe RXJS */
+interface User{
+  firstName:string,
+  lastName:string
+}
 
 @Component({
   selector: 'app-enfant',
@@ -15,9 +22,26 @@ export class EnfantComponent implements OnInit {
   @ViewChild('secondParagraphe',{static:true}) el: ElementRef<HTMLParagraphElement>
   constructor() { }
 
+  /**utilisation des pipe avant le subscribe pour modifier la donnée contenue dans l'observable */
+  personne: BehaviorSubject<User> = new BehaviorSubject(null)
+
   ngOnInit() {
     console.log(this.el)
     console.log(this.el.nativeElement.innerHTML)
+
+    /**Apres un appel http par exemple je next le resulta recu*/
+    this.personne.next({
+      lastName: 'pierre',
+      firstName : 'Girou'
+    })
+
+    this.personne.pipe(
+      filter((personne: User) =>  personne != null ), // ici si le user existe alors il passera a l'étape d'apres si il stop car il fait qu'il renvoi un boolean
+      map((personne: User)=> {
+        return `${personne.firstName} ${personne.lastName}`
+      }) // grace au pipe je modifi la donnée recu avant le subscribe
+    ).subscribe(data => console.log(data));
+    
   }
 
 }
